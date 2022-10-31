7f12eca2d9bf84c6ab4e90d4b67936db523fd382
Soroush Habibi <soroush.habibi.1382@gmail.com>
2022-10-31 19:44:01 +0330

    add addTask editTask importData & deleteTask method to DB class

--- db.js
+++ db.js
@@ -76,4 +76,117 @@ export default class DB {
         }
         return data;
     }
+    static addTask(title, completed) {
+        let data = [];
+        try {
+            data = JSON.parse(fs.readFileSync(process.env.DB_FILE, "utf-8"));
+        } catch (e) {
+            throw new Error("Can't Read or Parse DB file");
+        }
+
+        for (let i = 0; i < data.length; i++) {
+            if (data[i].title === title) {
+                throw new Error("This title used once");
+            }
+        }
+
+        data.push({
+            id: (data[data.length - 1].id + 1),
+            title: title,
+            completed: completed
+        });
+
+        try {
+            data = JSON.stringify(data);
+            fs.writeFileSync(process.env.DB_FILE, data);
+            return true;
+        } catch (e) {
+            throw new Error("Can't write on DB file");
+        }
+    }
+    static editTask(id, title, completed) {
+        id = Number(id);
+        let data = [];
+        if (id <= 0 || id !== parseInt(id)) {
+            throw new Error("Id must be a number bigger than 0");
+        }
+
+        try {
+            data = JSON.parse(fs.readFileSync(process.env.DB_FILE, "utf-8"));
+        } catch (e) {
+            throw new Error("Can't Read or Parse DB file");
+        }
+
+        for (let i = 0; i < data.length; i++) {
+            if (data[i].id === id) {
+                for (let j = 0; j < data.length; j++) {
+                    if (j !== i) {
+                        if (data[j].title === title) {
+                            throw new Error("This title used in another task");
+                        }
+                    }
+                }
+                data[i].title = title;
+                data[i].completed = completed;
+
+                try {
+                    data = JSON.stringify(data);
+                    fs.writeFileSync(process.env.DB_FILE, data);
+                    return true;
+                } catch (e) {
+                    throw new Error("Can't write on DB file");
+                }
+            }
+        }
+
+        return false;
+    }
+    static deleteTaskById(i) {
+        i = Number(i);
+        let data = [];
+        if (i <= 0 || i !== parseInt(i)) {
+            throw new Error("ID must be an integer bigger than 0");
+        }
+
+        try {
+            data = JSON.parse(fs.readFileSync(process.env.DB_FILE, "utf-8"));
+        } catch (e) {
+            throw new Error("Can't Read or Parse DB file");
+        }
+
+        for (let j = 0; j < data.length; j++) {
+            if (data[j].id === i) {
+                data.splice(j, 1);
+
+                try {
+                    data = JSON.stringify(data);
+                    fs.writeFileSync(process.env.DB_FILE, data);
+                    return true;
+                } catch (e) {
+                    throw new Error("Can't Write on DB file");
+                }
+            }
+        }
+
+        return false;
+    }
+    static importData(data) {
+        if (typeof data === 'string') {
+            try {
+                data = JSON.parse(data);
+            } catch (e) {
+                throw new Error("Invalid input");
+            }
+        }
+
+        if (data instanceof Array) {
+            try {
+                fs.writeFileSync(process.env.DB_FILE, JSON.stringify(data));
+            } catch (e) {
+                throw new Error("Can't write on DB file");
+            }
+        } else {
+            throw new Error("Invalid input");
+        }
+    }
 }
--- index.js
+++ index.js
@@ -8,16 +8,44 @@ console.clear();
 const okText = chalk.greenBright.bold;
 const errText = chalk.redBright.bold;
 
-try {
-    if (DB.createDB()) {
-        console.log(okText("DB created sucessfully"));
-    } else {
-        console.log(errText("DB already exists"));
-    }
-} catch (e) {
-    console.log(errText(e.message));
-}
+// try {
+//     if (DB.createDB()) {
+//         console.log(okText("DB created sucessfully"));
+//     } else {
+//         console.log(errText("DB already exists"));
+//     }
+// } catch (e) {
+//     console.log(errText(e.message));
+// }
 
-console.log(DB.searchElementById(2));
-console.log(DB.searchElementByTitle("book1"));
-console.log(DB.allData());
+// try {
+//     console.log(DB.searchElementById(2));
+//     console.log(DB.searchElementByTitle("book1"));
+//     console.log(DB.allData());
+// } catch (e) {
+//     console.log(e.message);
+// }
+
+// try {
+//     DB.addTask("book6", false);
+// } catch (e) {
+//     console.log(e.message);
+// }
+
+// try {
+//     console.log(DB.editTask(4, "book5", true));
+// } catch (e) {
+//     console.log(e.message);
+// }
+
+// try {
+//     console.log(DB.deleteTaskById(4));
+// } catch (e) {
+//     console.log(errText(e.message));
+// }
+
+// try {
+//     DB.importData("[{\"id\": 1, \"title\": \"book1\",   \"completed\": true}, {\"id\": 2,\"title\": \"book2\",\"completed\": false}]");
+// } catch (e) {
+//     console.log(errText(e.message));
+// }
